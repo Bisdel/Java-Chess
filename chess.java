9936f98305775bf472b57439c71e503dcc97803d
Bisdel <bricotte.leo@gmail.com>
2023-03-27 17:11:14 +0200

    modified:   chess.java

--- chess.java
+++ chess.java
@@ -12,18 +12,21 @@ public class chess {
             this.name = name;
             this.color = color;
         }
-        void pickAColor() {
+        public Player pickAColor(Player player) {
             System.out.println("What's your name ?");
-            String name = sc.nextLine();
-            System.out.println("Welcome " + name + " !\nWhich color would you play ? Enter 1 to play white, or 2 to play black.");
-            String color = sc.nextLine();
-            if (color == "W") {
-                System.out.println(name + " will play white. The convention in chess is white starts, so it's your turn !");
-            } else if (color == "B") {
-                System.out.println(name + " will play black. The convention in chess is white starts, so it's your opponent's turn.");
-            } else {
-                System.out.println("You didn't entered W or B. Please focus and pick W or B to choose your color.");
+            player.name = sc.nextLine();
+            System.out.println("Welcome " + player.name + " !\nWhich color would you play ? Enter W to play white, or B to play black.");
+            player.color = sc.nextLine();
+            while(player.color != "W" || player.color != "B"){
+                if (player.color == "W") {
+                    System.out.println(player.name + " will play white. The convention in chess is white starts, so it's your turn !");
+                } else if (player.color == "B") {
+                    System.out.println(player.name + " will play black. The convention in chess is white starts, so it's your opponent's turn.");
+                } else {
+                    System.out.println("You didn't entered W or B. Please focus and pick W or B to choose your color.");
+                }
             }
+            return player;    
         }
     }
     
@@ -87,9 +90,9 @@ public class chess {
     }
     public static void main(String[] args) {
         Player player1 = new Player("","");
-        player1.pickAColor();
+        player1 = player1.pickAColor(player1);
         Player player2 = new Player("","");
-        player2.pickAColor();
+        player2 = player2.pickAColor(player2);
         Board board = new Board();
         board.displayBoard();
     }
